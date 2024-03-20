650cbbbe41061b8aa63dfb98bb738a5aa5a7a490
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-03-20 14:20:27 -0300

    atvfunçãoareacirculo

---
+++ atvfunçãoáreacirculo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+float area_circulo(float raio)
+{
+    return M_PI * pow(raio, 2);
+}
+
+int main()
+{
+    float r, area;
+    scanf("%f", &r);
+    area = area_circulo(r);
+    printf("A área do círculo é: %f\n", area);
+    return 0;
+}
